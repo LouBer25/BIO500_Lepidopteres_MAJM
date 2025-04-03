d1c85ff8493e7a2cd8108bf5ec7d726d53619d98
LouBer25 <berm8376@usherbrooke.ca>
2025-04-02 22:48:19 -0400

    Correction de la fonction 6 obs_variable

--- Projet_final/6_fonction_obs_variable.R
+++ Projet_final/6_fonction_obs_variable.R
@@ -1,13 +1,14 @@
-# 5) Fonction qui remplace la valeur textuelle de la colonne obs_variable en valeur numérique
-# nécessite package dplyr
+# 5) Fonction qui remplace la valeur textuelle de la colonne obs_variable en valeur numérique dans lepido_new
 
-obs_variable <- function(tableau){
-  lepido_new <- lepido_new %>%  
-    mutate(obs_variable = recode(obs_variable,       #Fonction pour recoder la valeur textuel en valeur numérique
-                                 "occurrence" = 0,   #la valeur occurence sera remplacée par 0
-                                 "presence" = 1,    #la valeur presence sera remplacée par 1
-                                 "pr@#sence" = 1,   #la valeur prsesence écrite avec un "é" sera remplacée par 1
-                                 "abundance" = 2,     #la valeur abundance sera remplacée par 2
-                                 .default = NA_real_ #Si mal écrit ou pas de valeur remplacé par NA  
-    ))
+remplacer_obs_variable <- function(tableau) {
+  
+  # Relier les valeurs
+  trans_obs_variable <- c("occurrence" = 0,
+                          "presence" = 1, 
+                         "pr@#sence" = 1,
+                          "abundance" = 2)
+  
+  lepido_new$obs_variable <- trans_obs_variable[lepido_new$obs_variable] #Remplacage
+  
+  return(lepido_new) #retourner lepido_new
 }

e5b87958a9c898b58b6821e0556735902344e99f
LouBer25 <berm8376@usherbrooke.ca>
2025-04-06 21:15:27 -0400

    Mise au propre du script principal

--- Projet_final/0_script_principal_lepidopteres_modifié.R
+++ Projet_final/0_script_principal_lepidopteres_modifié.R
@@ -1,15 +1,9 @@
 #####ATTENTION#####
-#L'utilisation de ce script nécessite l'enregistrement des fichiers tels qu'ils sont organisés#
-
-# 1) Le dossier "Lepidopteres_BD" contient tous les fichiers de chaque année et doit être dans le fichier "Projet_final" dans votre explorateur de fichier
-# Extraire les fichiers
-
-####Vérification####
-# 2) Ouvrir avec Bloc notes le fichier "1883.csv": modifier l'entête "titre" par "title" (uniformiser), enregistrer, puis fermer.
+#L'utilisation de ce script nécessite l'enregistrement des fichiers tels qu'ils sont organisés, soit à partir du Git ou autre#
 
 
-# 3) Installez les packages "readr", ils seront utile pour importer toutes les données contenues dans les fichiers .csv.Exécutez ces lignes de codes
-### Installer les packages suivants pour compléter les prochaines étapes.
+# 1) Installez les packages suivants si ce n'est pas fait, ils seront utile pour importer toutes les données contenues dans les fichiers .csv
+### Ouvrir les librairies nécessaires
 
 #install.packages("dplyr")
 library(dplyr)              #Cela est plus optimisé que des boucle, sinon se sera trop long à générer
@@ -28,12 +22,12 @@ library(readr)
 
 
 
-# 4) Set Working Directory au dossier "Projet_final", ou exécuter la fonction suivante :
-setwd("C:/Users/Alex/Desktop/test")
+# 2) Set Working Directory au dossier "BIO500_Lepidopteres_MAJM", ou exécuter la fonction suivante :
+setwd("C:/Users/marbe/Desktop/UdeS Hiver 2025/Méthodes en écologie computationnelle/BIO500_Lepidopteres_MAJM")
 
 
-# 5) Exécutez les fonctions suivantes pour charger les fonctions qui seront utiles pour nettoyer les données.
-setwd("./Projet_final")
+# 3) Exécutez les fonctions suivantes pour charger les fonctions qui seront utiles pour nettoyer les données.
+setwd("./Projet_final")	#Ouvrir le Working Directory qui contient les fichiers sources des fonctions
 source("1_fonction_BD.R")
 source("3_fonction_formats_temporels.R")
 source("4_fonction_coordo_geographiques.R")
@@ -44,59 +38,51 @@ source("8_fonction_creation_tableau_taxonomie.R")
 source("9_fonction_itis_lepido.R")
 
 
-# 6) Exécutez la fonction suivante pour générer la base de données.
-##Le chemin d'accès à entrer correspond au chemin jusqu'au dossier Lepidopteres_BD créer précédemment
+# 4) Exécutez la fonction suivante pour générer la base de données
 lepido_BD <- Lepidopteres_BD("./Lepidopteres_BD")
 
 
-
-## À partir d'ici, travaillez avec le data frame lepido_new qui est exempt de duplicatas.
-
+## À partir d'ici, travaillez avec le data frame lepido_new qui est exempt de duplicatas##
 
 
-# 8) Exécutez cette fonction pour détecter et corriger les formats temporels sur le tableau lepido_new
+# 5) Exécutez cette fonction pour détecter et corriger les formats temporels sur le tableau lepido_new
 lepido_new <- corriger_dates(lepido_BD) ### erreur dans cette fonction, à corriger dans 1_fonctions_lepidopteres.R.
 
 
-# 9) Exécutez cette fonction pour corriger les coordonnées géographiques
+# 6) Exécutez cette fonction pour corriger les coordonnées géographiques
 lepido_new <- corriger_coordonnees(lepido_new)
 
 
-# 10) Fonction pour convertir les données dans les bons types (ex. boolléen, numérique, etc.)
+# 7) Fonction pour convertir les données dans les bons types (ex. boolléen, numérique, etc.)
 lepido_new <- convertir_types(lepido_new)   #fonctionne, mais à retravailler: dit que ça intègre des NA lors de la conversion automatique
 
 
-# 11) Exécutez cette fonction qui remplace la valeur textuelle de la colonne obs_variable en valeur numérique
+# 8) Exécutez cette fonction qui remplace la valeur textuelle de la colonne obs_variable en valeur numérique
 ##Besoin du package "dplyr" pour exécuter cette fonction
 lepido_new <- remplacer_obs_variable(lepido_new)
 
-# 12) Exécutez la fonction suivante pour générer la base de données sur les espèces
-##Le chemin d'accès à entrer correspond au chemin jusqu'au fichier taxonomie.csv directement qui devrait être dans le dossier Projet_final
+# 9) Exécutez la fonction suivante pour générer la base de données sur les espèces
 taxonomie_BD <- read.csv("../taxonomie_test.csv")
 
-# 13) Ajoute le code itis au tableau taxonomie
-##Le chemin d'accès à entrer correspond au chemin jusqu'au fichier taxonomie_BD.csv créer précédemment qui devrait être dans le dossier Projet_final
+# 10) Ajoute le code itis au tableau taxonomie
 taxonomie_table <- tableau_taxonomie("..")
 
-# 14) Ajoute le code itis au tableau lepido_new
-##Le chemin d'accès à entrer correspond au chemin jusqu'au dossier Projet_final créer précédemment
+# 11) Ajoute le code itis au tableau lepido_new
 lepido_new <- colonne_itis(getwd())
 
-# 15) Ouvrir la source pour écrire le fichier csv "lepido_final.csv"
+# 12) Ouvrir la source pour écrire le fichier csv "lepido_final.csv"
 source("10_fonction_csv_lepido_final.R")
 
-# 16) Créer le fichier csv "lepido_final.csv"
-##Le chemin d'accès à entrer correspond au chemin jusqu'au fichier lepido_final.csv qui devrait être dans le dossier Projet_final
+# 13) Créer le fichier csv "lepido_final.csv"
 ecrire_lepido_final("./lepido_final.csv")
 
-#17) 
-# 2) Ouverture de la librairie SQL
+# 14) Ouverture de la librairie SQL
 library(RSQLite)
 
-# 3) Connection au language SQL
+# 15) Connection au language SQL
 con <- dbConnect(SQLite(), dbname="donnee.db")
 
-# 4) Initialisation des tables
+# 16) Initialisation des tables pour effacer les tables si elles existent déjà
 dbSendQuery(con, "DROP TABLE IF EXISTS espece;")
 dbSendQuery(con, "DROP TABLE IF EXISTS date;")
 dbSendQuery(con, "DROP TABLE IF EXISTS source;")
@@ -105,7 +91,7 @@ dbSendQuery(con, "DROP TABLE IF EXISTS latitude;")
 dbSendQuery(con, "DROP TABLE IF EXISTS longitude;")
 dbSendQuery(con, "DROP TABLE IF EXISTS observation;")
 
-# 5) Création de la table espèces
+# 17) Création de la table espèces
 creer_espece <-
 	"CREATE TABLE espece (
 		observed_scientific_name	VARCHAR(100),
@@ -125,7 +111,7 @@ creer_espece <-
 	);"
 dbSendQuery(con, creer_espece)
 
-# 6) Création de la table date
+# 18) Création de la table date
 creer_date <-
 	"CREATE TABLE date (
 		year_obs		INTEGER(4),
@@ -136,7 +122,7 @@ creer_date <-
 	);"
 dbSendQuery(con, creer_date)
 
-# 7) Création de la table source
+# 19) Création de la table source
 creer_source <-
 	"CREATE TABLE source (
 		original_source		VARCHAR(20),
@@ -150,7 +136,7 @@ creer_source <-
 	);"
 dbSendQuery(con, creer_source)
 
-# 8) Création de la table abbondance
+# 20) Création de la table abbondance
 creer_abbondance <-
 	"CREATE TABLE abbondance (
 		obs_variable	VARCHAR(100),
@@ -158,20 +144,21 @@ creer_abbondance <-
 	);"
 dbSendQuery(con, creer_abbondance)
 
-# 9) Création de la table latitude et longitude
+# 21) Création de la table latitude
 creer_latitude <- 
   "CREATE TABLE latitude (
   lat REAL PRIMARY KEY
   );"
 dbSendQuery(con, creer_latitude)
 
+# 22) Création de la table longitude
 creer_longitude <- 
   "CREATE TABLE longitude (
   lon REAL PRIMARY KEY
   );"
 dbSendQuery(con, creer_longitude)
 
-# 10) Création de la table observation
+# 23) Création de la table observation
 creer_observation <- 
 	"CREATE TABLE observation (
 		id_observation   INTEGER,
@@ -191,10 +178,10 @@ creer_observation <-
 	);"
 dbSendQuery(con, creer_observation)
 
-# 11) Ouverture de la fonction pour la création de données
+# 24) Ouverture de la fonction pour la création de données
 source("11_fonction_creation_donnees.R")
 
-# 11) Assignation des données
+# 25) Assignation des données
 donnee_espece <- espece(getwd())
 donnee_date <- date(getwd())
 donnee_source <- source(getwd())
@@ -203,7 +190,7 @@ donnee_latitude <- latitude(getwd())
 donnee_longitude <- longitude(getwd())
 donnee_observation <- observation(getwd())
 
-# 12) Injection des données dans les tables
+# 26) Injection des données dans les tables
 dbWriteTable(con, append = TRUE, name = "espece", value = donnee_espece, row.names = FALSE)
 dbWriteTable(con, append = TRUE, name = "date", value = donnee_date, row.names = FALSE)
 dbWriteTable(con, append = TRUE, name = "source", value = donnee_source, row.names = FALSE)
@@ -212,7 +199,7 @@ dbWriteTable(con, append = TRUE, name = "latitude", value = donnee_latitude, row
 dbWriteTable(con, append = TRUE, name = "longitude", value = donnee_longitude, row.names = FALSE)
 dbWriteTable(con, append = TRUE, name = "observation", value = donnee_observation, row.names = FALSE)
 
-# 13) Requête SQL
+# 27) Requête SQL de test
   test <- "
 	SELECT id_observation, obs_variable, observed_scientific_name
 	FROM observation
@@ -221,6 +208,7 @@ dbWriteTable(con, append = TRUE, name = "observation", value = donnee_observatio
   test_test <- dbGetQuery(con, test)
 head(test_test)
 
-dbDisconnect(con) 
+# 28) Déconnection de SQL
+dbDisconnect(con)
 
 

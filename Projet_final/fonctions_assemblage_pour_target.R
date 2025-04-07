d3a2b8dc43e5fe6c6a97fbb21ff007627aeb356e
MaiteSimone <maite-simone.gauthier-benard@usherbrooke.ca>
2025-04-07 11:43:59 -0400

    Création des fonctions assemblage de données et création tables SQL

--- Projet_final/fonctions_assemblage_pour_target.R
+++ Projet_final/fonctions_assemblage_pour_target.R
@@ -8,11 +8,12 @@ lecture_donnees <- function(chemin_acces){
   library(furrr)              #Pour exécuter en parallèle la fonction "purrr"
   library(future)             #Exécuter furrr
   library(readr)
+  library(RSQLite)
   
-  # 2) Set Working Directory au dossier "BIO500_Lepidopteres_MAJM", ou exécuter la fonction suivante :
+  # a) Set Working Directory au dossier "BIO500_Lepidopteres_MAJM", ou exécuter la fonction suivante :
   setwd(chemin_acces)
 
-  # 3) Exécutez les fonctions suivantes pour charger les fonctions qui seront utiles pour nettoyer les données.
+  # b) Exécutez les fonctions suivantes pour charger les fonctions qui seront utiles pour nettoyer les données.
   setwd("./Projet_final")	#Ouvrir le Working Directory qui contient les fichiers sources des fonctions
   source("1_fonction_BD.R")
   source("3_fonction_formats_temporels.R")
@@ -24,15 +25,165 @@ lecture_donnees <- function(chemin_acces){
   source("9_fonction_itis_lepido.R")
 }
 
+# 2) Assemblage et nettoyage des données
+# chemin_acces = "./Lepidopteres_BD"
+assemblage_donnees <- function(chemin_acces){
+  
+  # a) Générer la base de données
+  lepido_BD <- Lepidopteres_BD(chemin_acces)
+
+  # b) Détecter et corriger les formats temporels
+  lepido_new <- corriger_dates(lepido_BD)
+
+  # c) Corriger les coordonnées géographiques
+  lepido_new <- corriger_coordonnees(lepido_new)
+
+  # d) Convertir les données dans les bons types de données
+  lepido_new <- convertir_types(lepido_new)
+
+  # e) Remplace la valeur textuelle de la colonne obs_variable en valeur numérique
+  ##Besoin du package "dplyr" pour exécuter cette fonction
+  lepido_new <- remplacer_obs_variable(lepido_new)
+
+  # f) Générer la base de données sur les espèces
+  taxonomie_BD <- read.csv("../taxonomie_test.csv")
+
+  # g) Ajouter le code itis au tableau taxonomie
+  taxonomie_table <- tableau_taxonomie("..")
+
+  # h) Ajouter le code itis au tableau lepido_new
+  lepido_new <- colonne_itis(getwd())
+
+  # i) Ouvrir la source pour écrire le fichier csv "lepido_final.csv"
+  source("10_fonction_csv_lepido_final.R")
+
+  # j) Créer le fichier csv "lepido_final.csv"
+  ecrire_lepido_final("./lepido_final.csv")
+}
 
 
+# 3) Création des tables SQL et injection des donnnées
 
+creation_SQL <- function(){
 
+  # a) Connection au language SQL
+  con <- dbConnect(SQLite(), dbname="donnee.db")
 
-# 2) Assemblage des données
+  # b) Initialisation des tables pour effacer les tables si elles existent déjà
+  dbSendQuery(con, "DROP TABLE IF EXISTS espece;")
+  dbSendQuery(con, "DROP TABLE IF EXISTS date;")
+  dbSendQuery(con, "DROP TABLE IF EXISTS source;")
+  dbSendQuery(con, "DROP TABLE IF EXISTS abbondance;")
+  dbSendQuery(con, "DROP TABLE IF EXISTS latitude;")
+  dbSendQuery(con, "DROP TABLE IF EXISTS longitude;")
+  dbSendQuery(con, "DROP TABLE IF EXISTS observation;")
 
+  # c) Création de la table espèces
+  creer_espece <-
+    "CREATE TABLE espece (
+		  observed_scientific_name	VARCHAR(100),
+	  	valid_scientific_name		VARCHAR(100),
+		  rank					VARCHAR(100),
+		  vernacular_fr			VARCHAR(100),
+		  kingdom				VARCHAR(100),
+		  phylum				VARCHAR(100),
+		  class					VARCHAR(100),
+		  `order`				VARCHAR(100),
+		  family				VARCHAR(100),
+		  genus					VARCHAR(100),
+		  species				VARCHAR(100),
+		  itis_code				INTEGER,
+		  PRIMARY KEY (observed_scientific_name)
+  	);"
+  dbSendQuery(con, creer_espece)
 
-# 3) Création des tables SQL
+ # d) Création de la table date
+  creer_date <-
+    "CREATE TABLE date (
+		  year_obs		INTEGER(4),
+		  day_obs		INTEGER(2),
+		  time_obs		VARCHAR(10),
+		  dwc_event_date	TIMESTAMP(20),
+		  PRIMARY KEY (dwc_event_date)
+  	);"
+  dbSendQuery(con, creer_date)
 
+  # e) Création de la table source
+  creer_source <-
+    "CREATE TABLE source (
+		  original_source		VARCHAR(20),
+		  creator			VARCHAR(100),
+		  title				VARCHAR(100),
+		  publisher			VARCHAR(100),
+		  intellectual_rights	VARCHAR(100),
+		  license			VARCHAR(40),
+		  owner				VARCHAR(100),
+		  PRIMARY KEY (title)
+	  );"
+  dbSendQuery(con, creer_source)
+
+  # f) Création de la table abbondance
+  creer_abbondance <-
+    "CREATE TABLE abbondance (
+		  obs_variable	VARCHAR(100),
+		  PRIMARY KEY (obs_variable)
+	  );"
+  dbSendQuery(con, creer_abbondance)
+
+  # g) Création de la table latitude
+  creer_latitude <- 
+   "CREATE TABLE latitude (
+    lat REAL PRIMARY KEY
+   );"
+  dbSendQuery(con, creer_latitude)
+
+  # h) Création de la table longitude
+  creer_longitude <- 
+   "CREATE TABLE longitude (
+    lon REAL PRIMARY KEY
+   );"
+  dbSendQuery(con, creer_longitude)
+
+  # i) Création de la table observation
+  creer_observation <- 
+    "CREATE TABLE observation (
+	  	id_observation   INTEGER,
+		  observed_scientific_name	VARCHAR(100),
+		  dwc_event_date 		TIMESTAMP(20) NOT NULL,
+		  obs_variable  VARCHAR(100),
+		  lat	INTEGER,
+		  lon INTEGER,
+		  title	VARCHAR(100),
+		  PRIMARY KEY (id_observation)
+		  FOREIGN KEY (observed_scientific_name) REFERENCES espece(observed_scientific_name),
+		  FOREIGN KEY (dwc_event_date) REFERENCES date(dwc_event_date),
+		  FOREIGN KEY (title) REFERENCES source(title),
+		  FOREIGN KEY (obs_variable) REFERENCES abbondance(obs_variable),
+		  FOREIGN KEY (lat) REFERENCES latitude(lat),
+		  FOREIGN KEY (lon) REFERENCES longitude(lon)
+	  );"
+  dbSendQuery(con, creer_observation)
+
+  # j) Ouverture de la fonction pour la création de données
+  source("11_fonction_creation_donnees.R")
+
+  # k) Assignation des données
+  donnee_espece <- espece(getwd())
+  donnee_date <- date(getwd())
+  donnee_source <- source(getwd())
+  donnee_abbondance <- abbondance(getwd())
+  donnee_latitude <- latitude(getwd())
+  donnee_longitude <- longitude(getwd())
+  donnee_observation <- observation(getwd())
+
+  # l) Injection des données dans les tables
+  dbWriteTable(con, append = TRUE, name = "espece", value = donnee_espece, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "date", value = donnee_date, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "source", value = donnee_source, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "abbondance", value = donnee_abbondance, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "latitude", value = donnee_latitude, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "longitude", value = donnee_longitude, row.names = FALSE)
+  dbWriteTable(con, append = TRUE, name = "observation", value = donnee_observation, row.names = FALSE)
+}
 
 # 4) Requêtes SQL

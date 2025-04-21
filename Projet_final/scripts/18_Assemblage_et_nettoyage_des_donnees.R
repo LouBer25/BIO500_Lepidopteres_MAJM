eb86e23c74751baed45e3b0d16824d78d39108ac
MaiteSimone <maite-simone.gauthier-benard@usherbrooke.ca>
2025-04-21 06:30:31 -0400

    remplacement du nouveau nom de dossier de tables_pour_SQL à tableaux_csv dans les fonctions des scripts 10, 11, 18, 8 et 9

--- Projet_final/scripts/18_Assemblage_et_nettoyage_des_donnees.R
+++ Projet_final/scripts/18_Assemblage_et_nettoyage_des_donnees.R
@@ -25,10 +25,10 @@ assemblage_donnees <- function(lepido_BD){
   lepido_new <- remplacer_obs_variable(lepido_new)
 
   # g) Ajouter le code itis au tableau taxonomie
-  taxonomie_table <- tableau_taxonomie("./tables_pour_SQL/taxonomie.csv")
+  taxonomie_table <- tableau_taxonomie("./tableaux_csv/taxonomie.csv")
 
   #Ouverture du fichier taxonomie_BD
-  taxonomie_BD <- read.csv("./tables_pour_SQL/taxonomie_BD.csv")
+  taxonomie_BD <- read.csv("./tableaux_csv/taxonomie_BD.csv")
 
   # h) Ajouter le code itis au tableau lepido_new
   lepido_final <- colonne_itis(lepido_new)
--- Projet_final/scripts/8_fonction_creation_tableau_taxonomie.R
+++ Projet_final/scripts/8_fonction_creation_tableau_taxonomie.R
@@ -2,12 +2,12 @@
 
 tableau_taxonomie <- function(chemin_acces){
 	taxonomie_BD <- read.csv(chemin_acces)
-	if (file.exists("./tables_pour_SQL/taxonomie_BD.csv")){
+	if (file.exists("./tableaux_csv/taxonomie_BD.csv")){
 		print ("Le document taxonomie_BD existe déjà dans le répertoire")
 	}else{
 	  source("./scripts/7_fonction_taxonomie_itis.R")
 		taxonomie_BD <- Transformation_en_code_taxo(taxonomie_BD)
-		write.csv(taxonomie_BD, file="./tables_pour_SQL/taxonomie_BD.csv")
+		write.csv(taxonomie_BD, file="./tableaux_csv/taxonomie_BD.csv")
 	}
 	return (taxonomie_BD)
 }
